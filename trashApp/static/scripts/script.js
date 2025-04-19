12abe3a440cde848eafbab25b4767a50a1fc3462
Sin <Licina_Sinan@teams.hs-ludwigsburg.de>
2025-04-19 12:13:25 +0200

    js, dashboard css angepasst

---
+++ trashApp/static/scripts/script.js
@@ -0,0 +1,15 @@
+function versteckeSystemstatus() {
+    var element = document.getElementById("systemstatusid");
+    element.classList.toggle("versteckt");
+}
+
+function versteckeLogbuch() {
+    var element = document.getElementById("logbuchid");
+    element.classList.toggle("versteckt");
+}
+
+
+function versteckeFuellstandtabelle() {
+    var element = document.getElementById("fuellstandTable");
+    element.classList.toggle("versteckt");
+}

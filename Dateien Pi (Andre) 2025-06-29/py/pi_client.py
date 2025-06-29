9d6a30b4961f52733c0938b38b946cbccad89906
AndreSchambach <schambach_andre@teams.hs-ludwigsburg.de>
2025-06-29 14:44:28 +0200

    "Dateien Pi Andre"  aktualisiert

--- Dateien Pi (Andre) 2025-06-29/py/pi_client.py
+++ Dateien Pi (Andre) 2025-06-29/py/pi_client.py
@@ -1,7 +1,7 @@
 import requests
 import os
-from PIL import Image #Pillow = bildbearbeitung
-import numpy as np #zum berechnen von tensoren (downgrade version für tflite)
+from PIL import Image
+import numpy as np
 import tflite_runtime.interpreter as tflite
 from datetime import datetime
 import subprocess
@@ -13,22 +13,23 @@ MODEL_PFAD = "/home/schambach/Trashy/Model/model_unquant.tflite"
 LABELS_PFAD = "/home/schambach/Trashy/Model/labels.txt"
 BILD_VERZEICHNIS = "/home/schambach/Trashy/Bilder/"
 NICHT_GESENDET_VERZEICHNIS = os.path.join(BILD_VERZEICHNIS, "nicht_gesendet")
-UUID = "4ca2e077-4e5e-42ab-8aa6-c15336fb88a9" #hier die uuid des entsprechend users (trashApp)
+PI_ID_PATH = "/home/schambach/Trashy/pi_id.txt"
 
-#klassifikationsmodell vorbereiten
+# PI-ID aus Datei lesen
+with open(PI_ID_PATH, "r") as f:
+    PI_ID = f.read().strip()
+
+# Modell vorbereiten
 interpreter = tflite.Interpreter(model_path=MODEL_PFAD)
-interpreter.allocate_tensors() #reserviert speicherplatz
+interpreter.allocate_tensors()
 input_index = interpreter.get_input_details()[0]["index"]
 output_index = interpreter.get_output_details()[0]["index"]
 
-#labels aus .txt in eine liste packen
-with open(LABELS_PFAD, "r") as datei:
-    zeilen = datei.readlines()
-    LABELS = []
-    for zeile in zeilen:
-        zeile_sauber = zeile.strip()
-        LABELS.append(zeile_sauber)
+# Labels einlesen
+with open(LABELS_PFAD, "r") as f:
+    LABELS = [zeile.strip() for zeile in f.readlines()]
 
+# Pins
 ROT_PIN = 17
 BLAU_PIN = 18
 TASTER_PIN = 12
@@ -40,11 +41,13 @@ lgpio.gpio_claim_input(h, TASTER_PIN, lgpio.SET_PULL_UP)
 
 def klassifizieren(pfad):
     bild = Image.open(pfad).resize((224, 224))
-    array = np.expand_dims(np.array(bild, dtype=np.float32) / 255.0, axis=0) #bild wird in numpy-array (float) umgewandelt... frag mich nicht, übersteigt meine gehaltsstufe
-    interpreter.set_tensor(input_index, array) #füttert das model mit bild-numpy-floaty-array-ding
+    array = np.expand_dims(np.array(bild, dtype=np.float32) / 255.0, axis=0)
+    interpreter.set_tensor(input_index, array)
     interpreter.invoke()
-    output = interpreter.get_tensor(output_index)[0] #gibt wahrscheinlichkeiten für die unterschiedlichen klassen/labels z.B. [0.1, 0.9, 0.0]
-    index = int(np.argmax(output)) # wählt die klasse/label mit der höchsten wahrscheinlichkeit
+    output = interpreter.get_tensor(output_index)[0]
+
+    index = int(np.argmax(output))
+    wahrscheinlichkeit = round(float(output[index]) * 100)
     label = LABELS[index]
 
     if label.lower() == "papier":
@@ -56,18 +59,19 @@ def klassifizieren(pfad):
         time.sleep(1)
         lgpio.gpio_write(h, BLAU_PIN, 0)
 
-    return label
+    return label, wahrscheinlichkeit
 
-def sende_bild(pfad, bildname, label, datum, uhrzeit):
+def sende_bild(pfad, bildname, label, datum, uhrzeit, wahrscheinlichkeit):
     try:
         with open(pfad, "rb") as datei:
             response = requests.post(SERVER_URL, files={
                 "bild": (bildname, datei, "image/jpeg"),
             }, data={
                 "label": label,
+                "wahrscheinlichkeit": wahrscheinlichkeit,
                 "datum": datum,
                 "uhrzeit": uhrzeit,
-                "uuid": UUID
+                "pi_id": PI_ID
             }, timeout=10)
 
         if response.status_code == 200:
@@ -87,18 +91,23 @@ def versuche_ausstehende_bilder_zu_senden():
             continue
 
         teile = os.path.splitext(datei)[0].split("_")
-        if len(teile) != 3:
+        if len(teile) != 4:
             print("Ungültiger Dateiname:", datei)
             continue
 
-        datum_raw, uhrzeit_raw, label = teile
+        datum_raw, uhrzeit_raw, label, wahrscheinlichkeit_str = teile
         datum = f"{datum_raw[:4]}-{datum_raw[4:6]}-{datum_raw[6:]}"
         uhrzeit = f"{uhrzeit_raw[:2]}:{uhrzeit_raw[2:4]}:{uhrzeit_raw[4:]}"
+        try:
+            wahrscheinlichkeit = int(wahrscheinlichkeit_str)
+        except ValueError:
+            print("Ungültige Wahrscheinlichkeit:", wahrscheinlichkeit_str)
+            continue
 
         pfad = os.path.join(NICHT_GESENDET_VERZEICHNIS, datei)
         neuer_pfad = os.path.join(BILD_VERZEICHNIS, datei)
 
-        if sende_bild(pfad, datei, label, datum, uhrzeit):
+        if sende_bild(pfad, datei, label, datum, uhrzeit, wahrscheinlichkeit):
             os.rename(pfad, neuer_pfad)
         else:
             print(f"erneuter Versuch fehlgeschlagen für: {datei}")
@@ -117,15 +126,15 @@ def aufnehmen_und_senden():
     temp_bildname = f"{zeitkompakt}.jpg"
     pfad = os.path.join(BILD_VERZEICHNIS, temp_bildname)
 
-    subprocess.run(["libcamera-still", "-o", pfad, "--timeout", "1000", "--width", "224", "--height", "224", "--nopreview"], check=True) #cheese :)
+    subprocess.run(["libcamera-still", "-o", pfad, "--timeout", "1000", "--width", "224", "--height", "224", "--nopreview"], check=True)
 
-    label = klassifizieren(pfad)
+    label, wahrscheinlichkeit = klassifizieren(pfad)
 
-    bildname = f"{zeitkompakt}_{label}.jpg"
+    bildname = f"{zeitkompakt}_{label}_{wahrscheinlichkeit}.jpg"
     neuer_pfad = os.path.join(BILD_VERZEICHNIS, bildname)
     os.rename(pfad, neuer_pfad)
 
-    if not sende_bild(neuer_pfad, bildname, label, datum, uhrzeit):
+    if not sende_bild(neuer_pfad, bildname, label, datum, uhrzeit, wahrscheinlichkeit):
         print("Speichere Bild lokal unter 'nicht_gesendet'")
         os.rename(neuer_pfad, os.path.join(NICHT_GESENDET_VERZEICHNIS, bildname))
 
